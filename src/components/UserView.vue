49346b4e49e90cb9f15fec42c8f4d1bfab1d2898
Paula Burghelea <paulaburghelea.github@gmail.com>
2023-10-12 12:08:12 +0100

    fist commit

--- src/components/UserView.vue
+++ src/components/UserView.vue
@@ -1,4 +1,14 @@
 <template>
+  <div class="featured">
+    <figure>
+      <img :src="featured.image" 
+        :alt="featured.title"
+      />
+      <figcaption><b>{{featured.title}}</b></figcaption>
+      <blockquote>{{featured.details}}</blockquote>
+
+    </figure>
+  </div>
   <div class="UserView">
     <ThumbGrid :projects2="projects"/>
   </div>
@@ -14,9 +24,37 @@ export default {
     ThumbGrid
   },
 
-  props: ["projects"]
+  props: ["projects"],
+
+  data() {
+    return {
+      featured: this.projects[0]
+    }
+  }
 };
 
 </script>
 
-<style></style>
+<style>
+figure,
+blockquote {
+  margin:0;
+  width: 100%;
+}
+.featured {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 30px;
+}
+.featured img {
+  width: 50%;
+  height: auto;
+  object-fit: cover;
+	aspect-ratio: 1/1;
+}
+figcaption,
+blockquote {
+  top: 0;
+  right: 0
+}
+</style>
